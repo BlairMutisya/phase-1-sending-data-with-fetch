dd82387264bc1974fd23aa05610959644eb77f82
BlairMutisya <tonnyblair09@gmail.com>
2024-04-06 12:49:50 +0300

    Adding data

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+        name: name,
+        email: email
+    };
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch(url, options)
+        .then(response => {
+
+        })
+        .then(data => {
+            const newId = data.id;
+            document.body.innerHTML += `<p>New user ID: ${newId}</p>`;
+            return newId;
+        });
+    }
+
